6defd697cd24397ec26800a6aac3b6add8c1bd00
frogman21-ai <frogm1708@gmail.com>
2026-06-25 20:13:05 +0700

    mencari luas

---
+++ nilai4.py
@@ -0,0 +1,7 @@
+panjang = 8
+lebar = 5
+luas = panjang * lebar
+print("Luas persegi panjang adalah:", luas)
+
+keliling = panjang * 2 + lebar * 2
+print("Keliling persegi panjang adalah:", keliling)
